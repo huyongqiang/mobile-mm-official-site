bf831187637ae1a42aa46ae638050b4bbec51180
Di Wang <wangdicoder@gmail.com>
2017-01-06 23:52:51 +1100

    app initial

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,3 +2,16 @@
  * Created by wangdi on 1/1/17.
  */
 'use strict';
+
+import React, {Component} from 'react';
+
+export default class App extends Component{
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
