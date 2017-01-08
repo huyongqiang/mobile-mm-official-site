7463cb862354cfb5e71269479f76bda20488ec33
Di Wang <wangdicoder@gmail.com>
2017-01-08 23:30:36 +1100

    add redux

---
+++ imports/ui/containers/AboutPage/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wangdi on 8/1/17.
+ */
+'use strict';
+
+import React, {Component} from 'react';
+
+export default class AboutPage extends Component{
+    render(){
+        return (
+            <div>
+                About
+            </div>
+        );
+    }
+}
---
+++ imports/ui/containers/AdminPage/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wangdi on 8/1/17.
+ */
+'use strict';
+
+import React, {Component} from 'react';
+
+export default class AdminPage extends Component{
+    render(){
+        return (
+            <div>
+                Admin
+            </div>
+        );
+    }
+}
