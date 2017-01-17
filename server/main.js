156542f990479df581d36d0440342685d8ca2ed2
Di Wang <wangdicoder@gmail.com>
2017-01-18 00:40:10 +1100

    modify import hierarchy

--- server/main.js
+++ server/main.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/collections/award_history';
-import '../imports/api/award_history';
+import '../imports/startup/server';
 
 Meteor.startup(() => {
   // code to run on server at startup
