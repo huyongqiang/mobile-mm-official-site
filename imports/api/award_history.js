ceb4828d706100208768b4a311954fa6ff720a77
Di Wang <wangdicoder@gmail.com>
2017-01-17 23:49:47 +1100

    add award collection

--- imports/api/award_history.js
+++ imports/api/award_history.js
@@ -1,3 +1,21 @@
 /**
  * Created by wangdi on 17/1/17.
  */
+'use strict';
+
+import {AwardHistory} from '../collections/award_history';
+import {Meteor} from 'meteor/meteor';
+
+if(Meteor.isServer){
+    Meteor.publish('award_history', function () {
+        return AwardHistory.find().fetch();
+    });
+}
+
+Meteor.methods({
+    'awards.insert'(time, contents){
+        AwardHistory.insert({
+            time, contents
+        });
+    }
+});
---
+++ imports/collections/award_history.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wangdi on 17/1/17.
+ */
+'use strict';
+
+import {Mongo} from 'meteor/mongo';
+
+export const AwardHistory = new Mongo.Collection('awards');
