da4a69196109835b19245c15c5c3a50266f4394b
Di Wang <wangdicoder@gmail.com>
2017-01-24 00:12:08 +1100

    join page

--- imports/ui/containers/JoinPage.js
+++ imports/ui/containers/JoinPage.js
@@ -12,13 +12,13 @@ export default class AwardPage extends Component{
             <div className="App-container">
                 <div className="join-page">
                     <h1>加入我们</h1>
-                    <form>
+                    <form onSubmit={this._submitOnPress.bind(this)}>
                         <div className="row">
                             <div className="left-part">
                                 <label>姓名 <span>*</span></label>
                             </div>
                             <div className="right-part">
-                                <input type="text" ref="name" placeholder="姓名" className="input-text join-text"/>
+                                <input type="text" ref="name" placeholder="姓名" className="input-text join-text" required="required"/>
                             </div>
                         </div>
                         <div className="row">
@@ -26,7 +26,7 @@ export default class AwardPage extends Component{
                                 <label>专业 <span>*</span></label>
                             </div>
                             <div className="right-part">
-                                <input type="text" ref="major" placeholder="专业" className="input-text join-text"/>
+                                <input type="text" ref="major" placeholder="专业" className="input-text join-text" required="required"/>
                             </div>
                         </div>
                         <div className="row">
@@ -34,7 +34,7 @@ export default class AwardPage extends Component{
                                 <label>兴趣方向 <span>*</span></label>
                             </div>
                             <div className="right-part">
-                                <select>
+                                <select className="option-select join-option">
                                     <option>手机App</option>
                                     <option>手机游戏</option>
                                     <option>设计</option>
@@ -47,12 +47,23 @@ export default class AwardPage extends Component{
                                 <label>加入原因 <span>*</span></label>
                             </div>
                             <div className="right-part">
-                                <textarea ref="mark" className="input-textarea join-textarea"/>
+                                <textarea ref="mark" className="input-textarea join-textarea" required="required"/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="left-part"/>
+                            <div className="right-part">
+                                <input type="submit" className="input-button btn-primary join-btn" value="提交"/>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         );
+    }
+
+    _submitOnPress(event){
+        event.preventDefault();
+        console.log('dasdasda');
     }
 }
