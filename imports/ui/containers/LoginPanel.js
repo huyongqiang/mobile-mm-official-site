e8fc07f3401e929c52ae20a0d2565f51739a22fa
Di Wang <wangdicoder@gmail.com>
2017-02-01 23:38:01 +1100

    admin login

---
+++ imports/ui/containers/LoginPanel.js
@@ -0,0 +1,23 @@
+/**
+ * Created by wangdi on 1/2/17.
+ */
+'use strict';
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Modal from '../components/Modal';
+
+export default class LoginPanel extends Component{
+
+    render(){
+        return (
+            <Modal>
+                <div>
+                    dasdasdas
+                </div>
+            </Modal>
+        );
+    }
+}
+
+ReactDOM.render(<LoginPanel />, document.getElementById('login'));
